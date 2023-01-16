69d271746aedff341dace0fbe19f51069c2f87fb
Andrés Carmelo Micalizzi <amicalizzi2005@gmail.com>
2023-01-16 13:22:41 -0300

    add: create_file.sh
    Creates a specified amount of files in a target path

---
+++ create_file.sh
@@ -0,0 +1,17 @@
+!/bin/bash
+
+# get target folder and number of files as input arguments
+folder=$1
+num_files=$2
+
+# create target folder if it doesn't exist
+if [ ! -d "$folder" ]; then
+  mkdir "$folder"
+fi
+
+# create specified number of files in the target folder
+for ((i=1; i<=num_files; i++)); do
+  touch "$folder/file$i"
+done
+
+echo "Successfully created $num_files files in $folder."
